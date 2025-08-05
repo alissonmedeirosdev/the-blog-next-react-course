5678abe7344fab277ff76963b055f9782ec07f1c
Alisson <alissonmelo.work@gmail.com>
2025-08-04 23:29:32 -0300

    adds light theme and dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+      <header>
+        <h1>Header</h1>
+      </header>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
 
-      <h1>Footer</h1>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </div>
   );
 }

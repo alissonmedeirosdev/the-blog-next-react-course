9c5572781e2cf9c0286b041183c2338341d9ffba
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-08-08 01:01:51 +0000

    Creating a Responsive PostList Grid

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,12 +1,41 @@
 import { postRepository } from "@/repositories/post";
+import { PostCoverImage } from "../PostCoverImage";
+import { PostHeading } from "../PostHeading";
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
 
   return (
-    <div>
+    <div className="grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div className="flex flex-col group gap-4" key={post.id}>
+            <PostCoverImage
+              linkProps={{ href: `/post/${post.slug}` }}
+              imageProps={{
+                src: post.coverImageUrl,
+                width: 1200,
+                height: 720,
+                alt: post.title,
+                priority: true,
+              }}
+            />
+
+            <div className="flex flex-col gap-4 sm:justify-center">
+              <time
+                className="block text-slate-300 text-sm/tight"
+                dateTime={post.createdAt}
+              >
+                {post.createdAt}
+              </time>
+              <PostHeading url="#" as="h2">
+                {post.title}
+              </PostHeading>
+
+              <p>{post.excerpt}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );

8093db2785a4a7befbcc79bdd9a52688bc71fdc4
Alisson Medeiros <alissonmelo.work@gmail.com>
2025-08-13 23:26:17 +0000

    Displaying the formatted date in our components

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostHeading } from "../PostHeading";
+import { formatDateTime, formatDistanceToNow } from "@/utils/format-datetime";
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
@@ -27,8 +28,9 @@ export async function PostsList() {
               <time
                 className="block text-slate-300 text-sm/tight"
                 dateTime={post.createdAt}
+                title={formatDistanceToNow(post.createdAt)}
               >
-                {post.createdAt}
+                {formatDateTime(post.createdAt)}
               </time>
               <PostHeading url={postLink} as="h2">
                 {post.title}

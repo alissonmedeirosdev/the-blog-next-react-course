0e12aea167cbaa273bf7b373742e1b91c8126dd3
Alisson <alissonmelo.work@gmail.com>
2025-09-03 00:23:55 -0300

    Separating the Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: {
@@ -25,9 +26,7 @@ export default function RootLayout({
 
           <div>{children}</div>
 
-          <footer>
-            <h1 className="font-bold text-6xl text-center py-8">Footer</h1>
-          </footer>
+          <Footer />
         </Container>
       </body>
     </html>

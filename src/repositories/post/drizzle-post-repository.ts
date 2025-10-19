2504e0de8e754defcaa9314aacdd0da49ae6caa0
Alisson <alissonmelo.work@gmail.com>
2025-10-19 12:01:58 -0300

    Continuando a criação do repositório DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -6,22 +6,48 @@ export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
+      where: (posts, { eq }) => eq(posts.published, true),
     });
 
     return posts;
   }
 
-  async findBySlugPublic(slug: string): Promise<PostModel> {}
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
 
-  async findAll(): Promise<PostModel[]> {}
+    if (!post) throw new Error("Post não encontrado para slug.");
 
-  async findById(id: string): Promise<PostModel> {}
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error("Post não encontrado para ID.");
+
+    return post;
+  }
 }
 
 (async () => {
-  const repo = new DrizzlePostRepository();
-
-  const posts = await repo.findAllPublic();
-
-  posts.forEach((post) => console.log(post.slug, post.published));
+  // os-desafios-do-trabalho-remoto-moderno
+  // 99f8add4-7684-4c16-a316-616271db199e
+  // const repo = new DrizzlePostRepository();
+  // const post = await repo.findBySlugPublic(
+  //   "os-desafios-do-trabalho-remoto-modernoaasd"
+  // );
+  // console.log(post.title);
 })();

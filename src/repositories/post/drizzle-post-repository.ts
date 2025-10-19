4e7edf33328e5ab0c56ea27dbac8e2f91973bcfb
Alisson <alissonmelo.work@gmail.com>
2025-10-19 10:43:18 -0300

    Criando o repositório DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,13 +1,11 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { drizzleDb } from "@/db/drizzle";
-import { postsTable } from "@/db/drizzle/schemas";
-import { desc } from "drizzle-orm";
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
     const posts = await drizzleDb.query.posts.findMany({
-      orderBy: desc(postsTable.createdAt),
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
     });
 
     return posts;
